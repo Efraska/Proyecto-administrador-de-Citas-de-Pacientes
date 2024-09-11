ca74ad6cb4fa9e8464a50b447d1886e51f1f9b41
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-09-11 17:49:14 -0600

    Creando el proyecto, objetos, clases y funciones

---
+++ js/app.js
@@ -0,0 +1,106 @@
+// Selectores
+const pacienteInput = document.querySelector('#paciente');
+const propietarioInput = document.querySelector('#propietario');
+const emailInput = document.querySelector('#email');
+const fechaInput = document.querySelector('#fecha');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#formulario-cita');
+
+// Eventos
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+
+formulario.addEventListener('submit', submitCita);
+
+// Objeto de Cita
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: ''
+}
+
+class Notificacion {
+
+    constructor({texto, tipo}) {
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrar();
+    }
+
+    mostrar() {
+        // Crear la notificacion
+        const alerta = document.createElement('DIV');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        // Eliminar alertas duplicadas
+        // const alertaPrevia = document.querySelector('.alert');
+        // if (alertaPrevia) {
+        //     alertaPrevia.remove();
+        // }
+
+        // Optional chaining
+        const alertaPrevia = document.querySelector('.alert');
+        alertaPrevia?.remove();
+
+        // Si es de tipo error, agregar una clase
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        // Mensaje de error
+        alerta.textContent = this.texto;
+
+        // Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario);
+
+        // Quitar despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+}
+
+class AdminCitas {
+    constructor() {
+        this.citas = [];
+    }
+
+    agregar(cita) {
+        this.citas = [...this.citas, cita];
+
+        console.log(this.citas);
+    }
+
+}
+
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value;
+}
+
+const citas = new AdminCitas();
+
+function submitCita(e) {
+    e.preventDefault();
+
+    // const { paciente, propietario, email, fecha, sintomas } = citaObj;
+    // if(paciente.trim() === '' || propietario.trim() === ''|| email.trim() === '' || fecha.trim() === '' || sintomas.trim() === '') {
+    //     console.log('Todos los campos son obligatorios');
+    //     return;
+    // }
+
+    if( Object.values(citaObj).some(valor => valor.trim() === '' )) {
+        new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
+        return;
+    }
+   
+    citas.agregar(citaObj);
+}
+
